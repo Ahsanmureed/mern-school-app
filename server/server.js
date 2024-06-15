ac79221db2433c151d288f7fa0106092a32dc3c8
Ahsanmureed <ahsanmureed00@gmail.com>
2024-06-15 07:57:33 +0500

    commit

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ import cookieParser from "cookie-parser";
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173",'https://mern-school-app.vercel.app'],
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials: true,
   })
